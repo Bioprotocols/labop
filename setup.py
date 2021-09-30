07503c6132a11241ac7eef3414bf6c6a6b1b377b
Daniel Bryce <danbryce410@gmail.com>
2021-09-30 16:06:01 -0500

    add ipython dep

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ setup(name='paml',
             'graphviz',
             'tyto',
             'numpy',
-            'openpyxl'
+            'openpyxl',
+            'ipython'
       ],
       packages=['paml', 'paml_md', 'paml.lib', 'paml_time', 'uml'],
       package_data={'paml': ['paml.ttl', 'lib/*.ttl'],
