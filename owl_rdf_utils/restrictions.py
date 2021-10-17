51b2dec57d3de74fc54e12e052846ebe87db23c4
Robert P. Goldman <rpgoldman@goldman-tribe.org>
2021-10-17 11:56:43 -0500

    Add owl_rdf_utils module.
    This contains code for handling bad restrictions and code for translating a graph into sorted ntriples format.

---
+++ owl_rdf_utils/restrictions.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Utilities for manipulating and correcting OWL files in RDF.
+"""
+
+__all__ = [
+    "all_restrictions",
+    "is_bad_restr",
+    "describe_bad_restr",
+    "translate_bad_restr",
+    "all_bad_restrictions",
+    "repair_graph",
+    "repair_all_bad_restrictions",
+]
+
+import argparse
+import logging
+import os
+import sys
+
+from copy import deepcopy
+
+import rdflib as rdf
+from rdflib import OWL, RDF, RDFS, Graph
+from rdflib.term import Node
+from rdflib.namespace import NamespaceManager
+
+from typing import List, Tuple, Optional, Any, Set
+
+Triple = Tuple[Any, Any, Any]
+
+# Properties of a restriction that indicate the actual constraint
+RELATIONS = [
+    OWL.allValuesFrom,
+    OWL.someValuesFrom,
+    OWL.minCardinality,
+    OWL.maxCardinality,
+    OWL.cardinality,
+    RDF.type,
+]
+# Properties of a restriction that we ignore when rewriting (because they are constraints ON
+# the restrictions rather than constraints on the restrictED
+IGNORE_PROPERTIES = [OWL.onProperty, RDFS.comment]
+
+RESTRICTIONS_QUERY = (
+    """
+    PREFIX owl: <%s>
+
+    SELECT ?r
+    {
+        ?r a owl:Restriction .
+    }
+"""
+    % rdf.OWL
+)
+
+LOGGER = logging.getLogger(__name__)
+
+
+def all_restrictions(graph: Graph) -> List[Node]:
+    """Return a list of the nodes naming owl:Restrictions."""
+    return [r["r"] for r in graph.query(RESTRICTIONS_QUERY)]
+
+
+def is_bad_restr(restr: Node, graph: Graph) -> bool:
+    """
+    Is this an ill-formed restriction?
+    """
+    rrs: Set[Node] = set()
+    rel: Node
+    for _r, rel, _x in graph.triples((restr, None, None)):
+        if rel in RELATIONS:
+            rrs.add(rel)
+    assert len(rrs) > 0, f"No components to restriction {restr}"
+    return len(rrs) > 1
+
+
+def describe_bad_restr(b: Node, g: Graph) -> None:
+    """Print description of a bad restriction to sys.stdout"""
+    nsm = rdf.namespace.NamespaceManager(g)
+    nsm.bind("owl", OWL)
+    triples = g.triples((b, None, None))
+    for x, _y, z in g.triples((b, RDF.type, None)):
+        print("%s a %s" % (x, z))
+    for _, y, z in triples:
+        if y != RDF.type:
+            print("\t%s %s" % (nsm.normalizeUri(y), nsm.normalizeUri(z)))
+
+
+def translate_bad_restr(b: Node, g: Graph) -> Tuple[List[Triple], List[Triple]]:
+    """
+    Return a list of RDF triples to be added and removed to repair a bad restriction.
+
+    The triples to be removed are the triples describing the bad restriction, and triples
+    indicating that some OWL class inherits from the bad restriction.
+
+    The triples to be added are triples that create a set of well-formed restrictions,
+    one per constraint, and triples that indicate subclasses of those restrictions.
+
+    Parameters
+    ----------
+    b : rdflib.term.Node
+       Node designating a restriction that is ill-formed and must be repaired.
+    g : rdflib.Graph
+       Graph containing the restriction to be repaired.
+
+    Returns
+    -------
+    to_add, to_delete : Tuple whose first element is a list of triples to be added
+      to `g`, and whose second is a list of triples to be removed.
+    """
+    comment: Optional[Any] = None
+    new_bnodes: List[rdf.BNode] = []
+    to_add: List[Triple] = []
+    to_delete: List[Triple] = [tuple for tuple in g.triples((b, None, None))]
+    nsm = rdf.namespace.NamespaceManager(g)
+    nsm.bind("owl", OWL)
+
+    def normalize(x):
+        return nsm.normalizeUri(x)
+
+    def find_children() -> List[Node]:
+        child_triples = g.triples((None, RDFS.subClassOf, b))
+        children = [x for x, _, _ in child_triples]
+        assert len(children) >= 1
+        return children
+
+    triples: List[Triple] = g.triples((b, None, None))
+    types: List[Node] = [z for _x, _y, z in g.triples((b, RDF.type, None))]
+    props: List[Node] = [z for _x, _y, z in g.triples((b, OWL.onProperty, None))]
+    comments: List[Node] = [z for _x, _y, z in g.triples((b, RDFS.comment, None))]
+    assert len(props) == 1
+    assert len(comments) <= 1
+    assert len(types) == 1
+    prop = props[0]
+    if comments:
+        comment = comments[0]
+
+    for _, y, z in triples:
+        if y not in set(IGNORE_PROPERTIES) | {RDF.type}:
+            bnode: rdf.BNode = rdf.BNode()
+            new_bnodes.append(bnode)
+            LOGGER.info(f"{nsm.normalizeUri(bnode)} a {nsm.normalizeUri(types[0])} ;")
+            to_add.append((bnode, RDF.type, types[0]))
+            LOGGER.info(f"\towl:onProperty {nsm.normalizeUri(prop)} ;")
+            to_add.append((bnode, OWL.onProperty, prop))
+            LOGGER.info(f"\t{nsm.normalizeUri(y)} {nsm.normalizeUri(z)}", end="")
+            to_add.append((bnode, y, z))
+            if comment:
+                LOGGER.info(f"\n\trdfs:comment {nsm.normalizeUri(comment)} .")
+                to_add.append((bnode, RDFS.comment, comment))
+            else:
+                LOGGER.info(".")
+    LOGGER.info("Children of this restriction are:")
+    for x in find_children():
+        LOGGER.info(f"\t{x}")
+        LOGGER.info(f"\tRemove {normalize(x)} rdfs:subClassOf {normalize(b)}")
+        to_delete.append((x, RDFS.subClassOf, b))
+        for nb in new_bnodes:
+            LOGGER.info(
+                f"\t{x} {nsm.normalizeUri(RDFS.subClassOf)} {nsm.normalizeUri(nb)}"
+            )
+            to_add.append((x, RDFS.subClassOf, nb))
+
+    return to_add, to_delete
+
+
+def all_bad_restrictions(g: Graph) -> List[Node]:
+    """List of all bad restrictions in graph."""
+    restrs = all_restrictions(g)
+    return [r for r in restrs if is_bad_restr(r, g)]
+
+
+def repair_all_bad_restrictions(g: rdf.Graph, bad: Optional[List[rdf.BNode]] = None):
+    if bad is None:
+        bad = all_bad_restrictions(g)
+    all_adds: List[Triple] = []
+    all_deletes: List[Triple] = []
+    for x in bad:
+        to_add, to_delete = translate_bad_restr(x, g)
+        all_adds += to_add
+        all_deletes += to_delete
+    for x in all_adds:
+        g.add(x)
+    for x in all_deletes:
+        g.remove(x)
+    return g
+
+
+def repair_graph(bad: List[Node], graph: Graph, dry_run: bool, file=sys.stdout):
+    if dry_run:
+        if file != sys.stdout:
+            LOGGER.addHandler(logging.StreamHandler(file))
+        for x in bad:
+            translate_bad_restr(x, graph)
+    else:
+        new_graph = repair_all_bad_restrictions(graph, bad)
+        new_graph.serialize(file)
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "action",
+        help="Action to perform.",
+        choices=["check", "repair"],
+        default="check",
+    )
+    ap.add_argument("input", help="File containing RDF graph to check")
+    ap.add_argument(
+        "--output", "-o", help="Write repaired RDF graph or check results here."
+    )
+    ap.add_argument("--verbose", "-v", dest="verbose", action="append_const")
+    ap.add_argument(
+        "--dry-run",
+        help="If repairing, just print the set of changes to be made, don't write output.",
+    )
+    ap.add_argument(
+        "--quiet",
+        help="Don't print descriptions of bad restrictions: just set exit flag.",
+        action="store_true",
+    )
+
+    values = ap.parse_args()
+    if values.verbose == 1:
+        LOGGER.setLevel(logging.INFO)
+    elif values.verbose >= 2:
+        LOGGER.setLevel(logging.DEBUG)
+    else:
+        LOGGER.setLevel(logging.WARNING)
+    # log to standard error
+    logging.basicConfig()
+
+    infile = values.input
+    assert os.path.exists(infile), f"No such file: {infile}"
+
+    graph = rdf.Graph()
+    graph.parse(infile)
+
+    bad = all_bad_restrictions(graph)
+
+    if ap.action == "check":
+        if bad:
+            print("Found bad restrictions in graph")
+            if not values.quiet:
+                to_file: bool = False
+                if hasattr(values, "output") and values.output:
+                    sys.stdout == open(values.output, "w")
+                    to_file = True
+                for b in bad:
+                    describe_bad_restr(b, graph)
+                if to_file:
+                    sys.stdout.close()
+            sys.exit(1)
+        sys.exit(0)
+    elif ap.action == "repair":
+        if not bad:
+            print("No repairs needed", file=sys.stderr)
+            sys.exit(1)
+        if hasattr(values, "output") and values.output:
+            with open(values.output, "w") as file:
+                repair_graph(bad, graph, values.dry_run, file)
+        else:
+            repair_graph(bad, graph, values.dry_run)
+
+
+if __name__ == "__main__":
+    main()
---
+++ owl_rdf_utils/to_sorted_ntriples.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+from typing import List, Optional, Union
+
+from rdflib import Graph
+
+NTRIPLES = "nt"
+
+__all__ = ['to_ntriples']
+
+
+def to_ntriples(graph: Graph) -> str:
+    """Return sorted n-triples representation of graph."""
+    # have RDFlib give us the ntriples as a string
+    nt_text = graph.serialize(format=NTRIPLES)
+    # split it into lines
+    lines = nt_text.splitlines(keepends=True)
+    # sort those lines
+    lines.sort()
+    # write out the lines
+    # RDFlib gives us bytes, so open file in binary mode
+    return join_lines(lines)
+
+
+def join_lines(lines: List[Union[bytes, str]]) -> str:
+    if isinstance(lines[0], bytes):
+        return b"\n".join([line.strip() for line in lines])
+    return "\n".join([line.strip() for line in lines])
+
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("input", help="File containing RDF graph to translate")
+    ap.add_argument("--output", "-o", help="Write RDF graph as sorted n-triples here.")
+    values = ap.parse_args()
+    infile = values.input
+    assert os.path.exists(infile), f"No such file: {infile}"
+
+    graph = Graph()
+    _, ext = os.path.splitext(infile)
+    print(ext)
+    format: Optional[str] = None
+    if ext == ".ttl":
+        format = 'turtle'
+    graph.parse(infile, format=format)
+    new_graph: Union[bytes, str] = to_ntriples(graph)
+    if isinstance(new_graph, bytes):
+        new_graph = new_graph.decode()
+    new_graph = new_graph.replace("\\n", "\n")
+    if hasattr(values, "output") and values.output:
+        with open(values.output, "w") as file:
+            print(new_graph, file=file)
+    else:
+        print(new_graph)
+
+
+if __name__ == "__main__":
+    main()
