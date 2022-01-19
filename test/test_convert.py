09b5b9037571cd38838b1d224b14a57f3940663d
Daniel Bryce <danbryce410@gmail.com>
2022-01-18 22:30:38 -0600

    Setup place to add conversion to opentrons.  Based on Autoprotocol conversion by removing specifics.

--- test/test_convert.py
+++ test/test_convert.py
@@ -1,26 +1,27 @@
 import filecmp
 import os
 import tempfile
+import unittest
 
-import pytest
 
 import paml
 import sbol3
 import tyto
 import uml
-import unittest
+
+from paml.execution_engine import ExecutionEngine
 
 from paml_convert.autoprotocol.autoprotocol_specialization import AutoprotocolSpecialization
-
 from paml_convert.autoprotocol.strateos_api import StrateosAPI, StrateosConfig
-from paml.execution_engine import ExecutionEngine
+
 from paml_convert.markdown.markdown_specialization import MarkdownSpecialization
 
+from paml_convert.opentrons.opentrons_specialization import OpentronsSpecialization
 
 class TestConvert(unittest.TestCase):
-    @pytest.mark.skip(reason="need to put strateos_secrets.json credentials on github first")
-    def test_igem_ludox(self):
 
+    @unittest.skip(reason="need to put strateos_secrets.json credentials on github first")
+    def test_igem_ludox_autoprotocol(self):
         #############################################
         # Set up the document
         print('Setting up document')
@@ -45,6 +46,49 @@ class TestConvert(unittest.TestCase):
         }
         autoprotocol_specialization = AutoprotocolSpecialization(autoprotocol_output, api, resolutions)
 
+
+        #############################################
+        # Execution Configuration
+        ee = ExecutionEngine(specializations=[autoprotocol_specialization])
+        agent = sbol3.Agent("test_agent")
+        parameter_values = [
+            paml.ParameterValue(parameter=protocol.get_input("wavelength"),
+                                value=uml.LiteralIdentified(value=sbol3.Measure(100, tyto.OM.nanometer)))
+        ]
+
+        #############################################
+        # Execute Protocol and Convert
+        execution = ee.execute(protocol, agent, id="test_execution", parameter_values=parameter_values)
+
+
+        #############################################
+        # Check outputs match
+
+        # Check Autoprotocol output
+        autoprotocol_comparison_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                'testfiles', 'igem_ludox_autoprotocol.json')
+        # Uncomment next two lines to write the rubric file (Careful!)
+        # with open(autoprotocol_comparison_file, "w") as out_file, open(autoprotocol_output) as in_file:
+        #     out_file.write(in_file.read())
+
+        print(f'Comparing against {autoprotocol_comparison_file}')
+        assert filecmp.cmp(autoprotocol_output, autoprotocol_comparison_file), "Autoprotocol files are not identical"
+        print('File identical with test file')
+
+    def test_igem_ludox_markdown(self):
+        #############################################
+        # Set up the document
+        print('Setting up document')
+        doc = sbol3.Document()
+        sbol3.set_namespace('https://bbn.com/scratch/')
+
+        #############################################
+        # Get the protocol
+        protocol_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testfiles", "igem_ludox_test.nt")
+        doc.read(protocol_file, 'nt')
+        protocol = doc.find("https://bbn.com/scratch/iGEM_LUDOX_OD_calibration_2018")
+
+
         #############################################
         # Markdown Configuration
         markdown_output = os.path.join(tempfile.gettempdir(), 'igem_ludox_markdown.md')
@@ -52,7 +96,7 @@ class TestConvert(unittest.TestCase):
 
         #############################################
         # Execution Configuration
-        ee = ExecutionEngine(specializations=[autoprotocol_specialization, markdown_specialization])
+        ee = ExecutionEngine(specializations=[markdown_specialization])
         agent = sbol3.Agent("test_agent")
         parameter_values = [
             paml.ParameterValue(parameter=protocol.get_input("wavelength"),
@@ -78,16 +122,54 @@ class TestConvert(unittest.TestCase):
         assert filecmp.cmp(markdown_output, markdown_comparison_file), "Markdown files are not identical"
         print('File identical with test file')
 
-        # Check Autoprotocol output
-        autoprotocol_comparison_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                                'testfiles', 'igem_ludox_autoprotocol.json')
+    def test_igem_ludox_opentrons(self):
+        #############################################
+        # Set up the document
+        print('Setting up document')
+        doc = sbol3.Document()
+        sbol3.set_namespace('https://bbn.com/scratch/')
+
+        #############################################
+        # Get the protocol
+        protocol_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "testfiles", "igem_ludox_test.nt")
+        doc.read(protocol_file, 'nt')
+        protocol = doc.find("https://bbn.com/scratch/iGEM_LUDOX_OD_calibration_2018")
+
+
+        #############################################
+        # Opentrons Configuration
+        opentrons_output = os.path.join(tempfile.gettempdir(), 'igem_ludox_opentrons.json')
+        opentrons_specialization = OpentronsSpecialization(opentrons_output)
+
+        #############################################
+        # Execution Configuration
+        ee = ExecutionEngine(specializations=[opentrons_specialization])
+        agent = sbol3.Agent("test_agent")
+        parameter_values = [
+            paml.ParameterValue(parameter=protocol.get_input("wavelength"),
+                                value=uml.LiteralIdentified(value=sbol3.Measure(100, tyto.OM.nanometer)))
+        ]
+
+        #############################################
+        # Execute Protocol and Convert
+        execution = ee.execute(protocol, agent, id="test_execution", parameter_values=parameter_values)
+
+
+        #############################################
+        # Check outputs match
+
+        # Check Opentrons output
+        # FIXME use something other than this markdown file below.
+        opentrons_comparison_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                'testfiles', 'igem_ludox_markdown.md')
         # Uncomment next two lines to write the rubric file (Careful!)
-        # with open(autoprotocol_comparison_file, "w") as out_file, open(autoprotocol_output) as in_file:
+        # with open(opentrons_comparison_file, "w") as out_file, open(opentrons_output) as in_file:
         #     out_file.write(in_file.read())
 
-        print(f'Comparing against {autoprotocol_comparison_file}')
-        assert filecmp.cmp(autoprotocol_output, autoprotocol_comparison_file), "Autoprotocol files are not identical"
+        print(f'Comparing against {opentrons_comparison_file}')
+        assert filecmp.cmp(opentrons_output, opentrons_comparison_file), "Markdown files are not identical"
         print('File identical with test file')
+
 
 if __name__ == '__main__':
     unittest.main()
