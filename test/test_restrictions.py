23a04c04baa9a206e2863addaadeed48b80f5516
Bryan Bartley <bryan.a.bartley@raytheon.com>
2022-01-26 16:43:35 -0800

    Add test to validate Restriction syntax for paml and uml

---
+++ test/test_restrictions.py
@@ -0,0 +1,44 @@
+import unittest
+import subprocess
+import owl_rdf_utils.restrictions
+import os
+import pytest
+
+
+class TestRestrictions(unittest.TestCase):
+
+    def test_bad_restrictions(self):
+        ontology_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'paml', 'paml.ttl')
+        try:
+            owl_rdf_utils.restrictions.main('check', ontology_file_name, None, quiet=True)
+        except SystemExit as e:
+            exit_code = str(e)
+            self.assertEqual(exit_code, '1')
+
+    def test_good_restrictions(self):
+        ontology_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'paml-revised.ttl')
+        try:
+            owl_rdf_utils.restrictions.main('check', ontology_file_name, None)
+        except SystemExit as e:
+            exit_code = str(e)
+            self.assertEqual(exit_code, '0')
+
+    def test_paml_actual(self):
+        ontology_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'paml-revised.ttl')
+        try:
+            owl_rdf_utils.restrictions.main('check', ontology_file_name, None, quiet=True)     
+        except SystemExit as e:
+            exit_code = str(e)
+            self.assertEqual(exit_code, '0')
+
+    def test_uml_actual(self):
+        ontology_file_name = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'uml-revised.ttl')
+        try:
+            owl_rdf_utils.restrictions.main('check', ontology_file_name, None, quiet=True)     
+        except SystemExit as e:
+            exit_code = str(e)
+            self.assertEqual(exit_code, '0')
+
+
+if __name__ == '__main__':
+    unittest.main()
