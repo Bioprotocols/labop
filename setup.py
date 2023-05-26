7a76a2edf6bc5259358677bbebf86a300b69eda6
Daniel Bryce <danbryce410@gmail.com>
2023-05-26 11:20:54 -0500

    remove duplicate setup.py key

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         "container-ontology @ https://github.com/rpgoldman/container-ontology/tarball/main",
         "xarray>=0.20.2",
     ],
-    python_requires=">=3.8",
     tests_require=test_deps,
     extras_require=extras,
     packages=[
